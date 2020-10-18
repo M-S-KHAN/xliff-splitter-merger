e48299423cff5c6fee098c96164256d798b7815f
M-S-KHAN <sajawalkhan111@gmail.com>
2020-10-19 00:52:21 +0500

    Updated Files with Added comments

--- merge.py
+++ merge.py
@@ -12,9 +12,10 @@ def merge(path):
 
     print("Processing...")
 
-    base_dir = path
+    base_dir = path    #path of the directory which contains all the files that need to be merged 
     file_paths = []
 
+    #It will take all the paths from base_dir and append them in file_path list
     for path in glob.glob(f'{base_dir}/*'):
         file_paths.append(path)
 
@@ -24,6 +25,7 @@ def merge(path):
 
     main_tree = etree.iterparse(file_paths[0], strip_cdata=False)
 
+    #This loop is used to remove all the originals or source name from the tags in the file
     for _, el in main_tree:
         _, _, el.tag = el.tag.rpartition('}')
 
@@ -36,6 +38,7 @@ def merge(path):
     source_lang = None
     target_lang = None
 
+    #This loop will iterate over all the root elements and will store source and target language and will also remove the body element so that we can have a header template for merged file
     for element in main_root.iter():
 
         if element.tag == 'file':
@@ -51,9 +54,10 @@ def merge(path):
 
     ##########        REMOVING BODY ELEMENTS FROM TEMPLATE        ##########
 
+    #enumerate allows us to return multiple values 
     for i, file_path in enumerate(file_paths):
 
-        original_dict_items = []
+        original_dict_items = []  #It will contain the filename, count of body element, and root of file with body elements removed and will store this list in dictionary with a unique key
 
         tree = etree.iterparse(file_path, strip_cdata=False)
 
@@ -67,35 +71,43 @@ def merge(path):
         for pair in file_ref.items():
             if pair[0] == 'original':
                 original_temp = pair[1]
-        # import pdb; pdb.set_trace()
+        
+        #Used to get the filename from the file path
         original_dict_items.append(file_path.split('\\')[-1])
 
         for element in root.iter():
-
+            
+            #Here we are checking if source language and target language of all the provided files are same. If not give exception
             if element.tag == 'file':
                 for pair in element.items():
                     if pair[0] == 'source-language' and not pair[1] == source_lang:
                         raise Exception(f"ERROR: Source Language Mismatch in file {file_path.split('/')[-1]}")
                     elif pair[0] == 'target-language' and not pair[1] == target_lang:
                         raise Exception(f"ERROR: Target Language Mismatch in file {file_path.split('/')[-1]}")
-
+            
+            
             if element.tag == 'body':
+                #Get the children count of 'body' element and append it in the list
                 original_dict_items.append(len(element.getchildren()))
                 for body_element in element.getchildren():
+                    #Copy the body elements and then append them in main root body
                     main_body.append(body_element.__copy__())
                 element.clear()
 
-        original_dict_items.append(etree.tostring(root.__copy__()))
+        original_dict_items.append(etree.tostring(root.__copy__())) #Will append the root in 'original_dict_items' with body elements removed
 
-        originals_dict[i] = original_dict_items
+        originals_dict[i] = original_dict_items  #Will store the list(Defined above) in the dictionary
 
+    #Creating the directory to contain the merged file
     if not os.path.exists('merged'):
         os.makedirs('merged')
 
+    #Creating the merged file to store the general header and bodies of all the input files
     with open("merged/merged.xliff", 'wb') as doc:
         doc.write(etree.tostring(main_root, method='xml', encoding='utf-8', standalone=False, xml_declaration=True))
 
-
+    #Storing the dictionary(originals_dict) in a pickle file
+    #Pickle file is used to store the object as it is and then the object can be easily retrived later
     with open("merged/originals.pkl", 'wb') as doc:
         pickle.dump(originals_dict, doc)
 
--- split.py
+++ split.py
@@ -12,42 +12,47 @@ def split(utils_path, split_dir):
     
     print("Processing...")
 
-    pickle_path = f"{utils_path}/originals.pkl"
-    merged_file_path = f"{utils_path}/merged.xliff"
-    split_files_base_directory = split_dir
+    pickle_path = f"{utils_path}/originals.pkl"   #Contains the pickle file path
+    merged_file_path = f"{utils_path}/merged.xliff"    #Contains the path of merged file        
+    split_files_base_directory = split_dir         #Contains the path in which split files will be stored
 
     file_to_read = open(pickle_path, "rb")
 
-    loaded_dictionary = pickle.load(file_to_read)
-    main_tree = etree.iterparse(merged_file_path, strip_cdata=False)
+    loaded_dictionary = pickle.load(file_to_read)   #Loading the dictionary stored in pickle file
+    main_tree = etree.iterparse(merged_file_path, strip_cdata=False)  #In order to save the Merged file main tree
 
+    #This loop is used to remove all the originals or source name from the tags in the file
     for _, el in main_tree:
         _, _, el.tag = el.tag.rpartition('}')
 
     main_root = main_tree.root
 
+    #This loop will iterate over all the elements of the root
     for element in main_root.iter():
 
         if element.tag == 'body':
 
-            trans_object_iterator = element.iterchildren()
-            loaded_dictionary_values = loaded_dictionary.values()
+            trans_object_iterator = element.iterchildren()  #Will contains the iterator of 'body' element
+            loaded_dictionary_values = loaded_dictionary.values()  #Stores all the values in the dictionary
 
             for value in loaded_dictionary_values:
 
                 file_name = value[0]
-                object_count = value[1]
-                current_root = etree.fromstring(value[2])
+                object_count = value[1]         #Number of children of the 'body' element
+                current_root = etree.fromstring(value[2])   #Contains the root with body elements removed 
 
                 for _, el in current_root:
                     _, _, el.tag = el.tag.rpartition('}')
 
+                #Will iterate over all the elements in current_root
                 for current_element in current_root.iter():
                     if current_element.tag == 'body':
+                        #This loop will iterate over all the body elements that we retrived from the dictionary
                         for j in range(object_count):
-                            next_trans_object = trans_object_iterator.__next__()
-                            current_element.append(next_trans_object.__copy__())
+                            next_trans_object = trans_object_iterator.__next__()    #This will contain the reference of the 'body' element children and then traverse them one by one using next function
+                            current_element.append(next_trans_object.__copy__())    #This will append all trans units or children of 'body element' that were removed during merging
 
+                #Will create a new file with same name to store the split files after processing
                 with open(os.path.join(split_files_base_directory, file_name), 'wb') as doc:
                     doc.write(etree.tostring(current_root, method='xml', encoding='utf-8', standalone=False, xml_declaration=True))
 
